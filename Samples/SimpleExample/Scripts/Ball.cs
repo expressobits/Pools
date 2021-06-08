b8309615e46d2cbde41c4b456fd56101374d31b6
Rafael Morais Correa <scriptsengineer@gmail.com>
2021-06-08 19:31:15 -0300

    Add simple example

---
+++ Samples/SimpleExample/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using ExpressoBits.Pools;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour {
+    
+    private Rigidbody m_Rigidbody;
+    private Renderer m_Renderer;
+    
+    private void Awake () {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    private void Start() {
+        m_Renderer.material.color = Random.ColorHSV();
+        m_Rigidbody.velocity = Vector3.zero;
+        m_Rigidbody.angularVelocity = Vector3.zero;
+        m_Rigidbody.angularDrag = 0f;
+    }
+
+    // public void OnPoolerDisable()
+    // {
+    //     //Example use interface IPooler
+    // }
+    //
+    // public void OnPoolerEnable()
+    // {
+    //     //Example use interface IPooler
+    //     m_Renderer.material.color = Random.ColorHSV();
+    //     m_Rigidbody.velocity = Vector3.zero;
+    //     m_Rigidbody.angularVelocity = Vector3.zero;
+    //     m_Rigidbody.angularDrag = 0f;
+    // }
+
+}
---
+++ Samples/SimpleExample/Scripts/Destroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    public bool isEnablePool;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(isEnablePool){
+            this.DestroyInPool(collider.gameObject);
+        }else{
+            Destroy(collider.gameObject);
+        }
+        
+    }
+    
+}
